401be525ec603ab2563251e598aea8358e11b87c
ahmedAalii <ahmedamuhammed2030@gmail.com>
2025-04-15 19:46:53 +0200

    Updated code for feature X

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Layout from './Components/Layout/Layout';
-import Home from './Components/home/home';
 import Conference from './Components/Conference/Conference';
 import About from './Components/About/About';
 import Events from './Components/Events/Events';
 import Joinus from './Components/Joinus/Joinus';
 import Login from './Components/Login/Login';
 import Notfound from './Components/Notfound/Notfound';
+import Home from './Components/Home/Home';
 
 
 let x = createBrowserRouter([
